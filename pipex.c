3c5e60c6af7121556ccfbf383f7ed56f50ce827f
goldishmthfckz <124591796+goldishmthfckz@users.noreply.github.com>
2024-02-07 11:26:02 +0100

    Add files via upload

--- pipex.c
+++ pipex.c
@@ -12,64 +12,74 @@
 
 #include "pipex.h"
 
-//char	*find_path(char *cmd, char **env)
-//{
-//	char	**allpath;
-//	char	**split_cmd;
-//	char	*path_part;
-//	char	*exec;
+char	*find_path(char *cmd, char **env)
+{
+	char	**allpaths;
+	char	**split_cmd;
+	char	*path;
+	char	*exec;
+	int		i;
 
+	i = 0;
+	while (ft_strnstr(env[i], "PATH", 4) == 0)
+		i++;
+	return (cmd);
+}
 
-//	return (cmd);
-//}
+void	execute_cmd(char *cmd, char **env)
+{
+	char	**split_cmd;
+	char	*path;
+	int	i;
 
-//void	execute_cmd(char *cmd, char **env)
-//{
-//	char	**split_cmd;
-//	char	*path;
-//	int	i;
+	**split_cmd = ft_split(cmd, ' ');
+	path = find_path(cmd, env);
+	if (execve(path, split_cmd, env) == -1)
+	{
+		ft_putstr_fd("Pipex: command not found: ", 2);
+		ft_putendl_fd(split_cmd[0], 2);
+		ft_freelist(split_cmd[i]);
+		exit(1);
+	}
+}
 
-//	**split_cmd = ft_split(cmd, ' ');
-//	path = find_path(cmd, env);
-//	if (execve(path, split_cmd, env) == -1)
-//	{
-//		ft_putstr_fd("Pipex: command not found: ", 2);
-//		ft_putendl_fd(split_cmd[0], 2);
-//		ft_freelist(split_cmd[i]);
-//		exit(1);
-//	}
-//}
+void	child(char **av, int *tube, char **env)
+{
+	int	infile;
 
-//void	child(char **av, int *tube, char **env)
-//{
-//	int	infile;
+	ft_printf("entree ds le child\n");
+	infile = open(av[1], 0);
+	dup2(infile, 0);
+	dup2(tube[1], 1);
+	close(tube[0]);
+	execute_cmd(av[2], env);
+}
 
-//	infile = open(av[1], 0);
-//	dup2(infile, 0);
-//	dup2(tube[1], 1);
-//	close(tube[0]);
-//	execute_cmd(av[2], env);
-//}
+//ouvre
+void	parent(char **av, int *tube, char **env)
+{
+	int	outfile;
 
-//void	parent(char **av, int *tube, char **env)
-//{
-//	int	outfile;
+	ft_printf("entree ds le parent\n");
+	outfile = open(av[4], 0);
+	dup2(outfile, 1);
+	dup2(tube[0], 0);
+	close(tube[1]);
+	execute_cmd(av[3], env);
+}
 
-//	outfile = open(av[4], 0);
-//	dup2(outfile, 1);
-//	dup2(tube[0], 0);
-//	close(tube[1]);
-//	execute_cmd(av[3], env);
-//}
-
-//si y a pas 4 arg, sortir
+//si y a pas 4 argv, affichier pas bn nb d'arg
+//si y a bien 4 argv ;
+//> pipe
+//> si ya erreur de pipe, exit tt le programme
+//> fork
+//> si ya erreur de fork, exit tt le programme
+//> sinon rentre ds processus parent puis enfant
 int	main(int ac, char **av, char **env)
 {
 	int	tube[2];
 	pid_t	pid;
 
-	(void)av;
-	(void)env;
 	if (ac == 5)
 	{
 		ft_printf("Bon nombre d'arguments (4)\n");
@@ -80,11 +90,9 @@ int	main(int ac, char **av, char **env)
 		if (pid == -1)
 			exit(1);
 		else if (pid == 0)
-			ft_printf("entree ds le child\n");
-			//child(av, tube, env);
+			child(av, tube, env);
 		else
-			ft_printf("entree ds le parent\n");
-			//parent(av, tube, env);
+			parent(av, tube, env);
 	}
 	else
 		ft_printf("madatory : Pas le bon nombre d'arguments\n");
